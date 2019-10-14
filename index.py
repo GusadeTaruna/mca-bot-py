373fd619ec47224ed23bf232b410b20bd74e7b9f
GusadeTaruna <38973744+GusadeTaruna@users.noreply.github.com>
2019-10-14 18:31:55 +0800

    pertama

---
+++ index.py
@@ -0,0 +1,29 @@
+import json
+from flask import Flask, request, make_response, jsonify
+import mysql.connector
+from mysql.connector import Error
+import datetime
+import calendar
+
+app = Flask(__name__)
+log = app.logger
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    """This method handles the http requests for the Dialogflow webhook
+    This is meant to be used in conjunction with the Banking Dialogflow agent
+    """
+    req = request.get_json(silent=True, force=True)
+    try:
+        action = req.get('result').get('action')
+    except AttributeError:
+        return 'json error'
+
+    if action == 'percakpan':
+        res = 'Webnya mau'
+
+    return make_response(jsonify({"speech": res}))
+
+
+if __name__ == '__main__':
+    app.run()
